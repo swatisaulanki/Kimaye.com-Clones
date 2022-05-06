f51764a1a35372c6defff1f5bc2cdfa90250da8a
Nirbhay Pratap Singh <nirbhaydalnia@gmail.com>
2022-05-06 12:51:58 +0530

    ourstory,why kimaye...!

---
+++ kimaye/src/components/Whykimaye.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import pix1 from './img/pix1.png'
+import pix2 from './img/pix2.png'
+import pix3 from './img/pix3.png'
+import pix4 from './img/pix4.png'
+import pix5 from './img/pix5.png'
+import pix6 from './img/pix6.png'
+import pix7 from './img/pix7.png'
+import pix8 from './img/pix8.png'
+import pix9 from './img/pix9.png'
+import pix10 from './img/pix10.png'
+import pix11 from './img/pix11.png'
+import pix12 from './img/pix12.png'
+
+
+export const Whykimaye = () => {
+  return (
+      <>
+      <div style={{width:"100%",margin:"auto"}}>
+          <img src={pix1} style={{marginTop:"-5px"}} />
+          <img src={pix2} style={{marginTop:"-5px"}} />
+          <img src={pix3} style={{marginTop:"-5px"}} />
+          <img src={pix4} style={{marginTop:"-5px"}} />
+          <img src={pix5} style={{marginTop:"-5px"}} />
+          <img src={pix6} style={{marginTop:"-5px"}} />
+          <img src={pix7} style={{marginTop:"-5px"}} />
+          <img src={pix8} style={{marginTop:"-5px"}} />
+          <img src={pix9} style={{marginTop:"-5px"}} />
+          <img src={pix10} style={{marginTop:"-5px"}} />
+          <img src={pix11} style={{marginTop:"-5px"}} />
+          <img src={pix12} style={{marginTop:"-5px"}} />
+      
+      </div>
+      </>
+    
+  )
+}
